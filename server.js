284d580cf8825f9cd7cca9cf5e05454a94cc7bd6
August F. Geier V <AGDevX@gmail.com>
2023-01-31 23:47:08 -0500

    livereload

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
 const open = require('open');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(connectLivereload());
 app.use(express.static('src'));
 
 app.get('/', (req, res) => {
@@ -15,3 +19,6 @@ app.listen(PORT, async () => {
 	console.log(`Server listening on port ${PORT}`);
 	await open(`http://localhost:${PORT}`);
 });
+
+const livereloadServer = livereload.createServer();
+livereloadServer.watch(path.join(__dirname, 'src'));
